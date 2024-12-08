0ae0c2ec8becda4b72e60f1b285737d108dee5a2
RAMEEZ RAZA <rameezrz25@gmail.com>
2024-12-08 09:08:58 +0530

    Time: 55 ms (20.10%) | Memory: 74.6 MB (23.18%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -5,12 +5,16 @@ public:
         set<int> st;
 
         for(auto num : nums){
+
+            // agar set find me last end mila  matlab nhi hai to insert kr do 
             if(st.find(num) == st.end()){
                 st.insert(num);
             }
+            // iska matlab hai pehle koi number
             else
                 return true;
     }
+        // loop complete ho gya matlab sare unique hi the
         return false;
 
         
